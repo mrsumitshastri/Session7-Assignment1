8dfc9f3c9d4951d2f0e1f9dbacf746035ff4c27e
Sumit Shastri <mrsumitshastri@gmail.com>
2016-03-13 19:30:29 +0530

    fdsf
    fdsf

--- src/com/Session7/Assignment1/MainClass.java
+++ src/com/Session7/Assignment1/MainClass.java
@@ -2,6 +2,10 @@ package com.Session7.Assignment1;
 
 public class MainClass {
 
+	/*
+	 * Handle ArrayIndexOutOfBounds Exception. For example if array is having only 5 elements and
+	 * we are trying to display 7th element then it should throw exception.
+	 */
 	public static void main(String[] args) {
 		int[] arr =  {1,2,3,4,5};
 		
